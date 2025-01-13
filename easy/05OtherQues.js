d7b05f2cf24adcdb4c9e2aa2c0989722960d7152
Priyanka Kumari <83893869+priyankaakumari10@users.noreply.github.com>
2025-01-13 22:13:39 +0530

    update

--- easy/05OtherQues.js
+++ easy/05OtherQues.js
@@ -8,4 +8,14 @@ const ans2 = arr.filter((n)=> n%2===0);
 
 // Check if there’s a number divisible by both 3 and 5
 const ans3 = arr.filter((n)=> n%3===0 && n%5===0);
-console.log(ans3)
+console.log(ans3);
+
+// check product out of stock
+const products = [
+    { name: 'Shoes', stock: 5 },
+    { name: 'Hat', stock: 0 },
+    { name: 'Bag', stock: 3 }
+];
+
+const product_out_of_stock = products.filter((pro)=> pro.stock<=0)
+console.log(product_out_of_stock)
